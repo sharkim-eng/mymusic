5db0860119229c210c2d44a2ea5bb39d5f2e417b
sharkim-eng <waferman6@gmail.com>
2026-08-31 19:14:10 +0900

    Add verified direct YouTube Music ID pack

---
+++ direct-ids-pack.js
@@ -0,0 +1,27 @@
+(() => {
+  // Verified YouTube / YouTube Music video IDs.
+  // Key format: normalized title|||normalized artist
+  window.SHARK_DIRECT_IDS = Object.assign(window.SHARK_DIRECT_IDS || {}, {
+    'no one noticed|||the marías': 'Qn8F_u0vBNI',
+    "i don't know you|||the marías": 'YzKM5g_FwYU',
+    'hush|||the marías': 'jFy03i_LRO8',
+    'only in my dreams|||the marías': 'qrqywuDWz_Q',
+    'tailwhip|||men i trust': '9IZKcb3LndA',
+    'myth|||beach house': 'FuvWc3ToDHg',
+    'chamber of reflection|||mac demarco': 'kz9jhG963no',
+    'my kind of woman|||mac demarco': 'wIuBcb2T55Q',
+    'crystalised|||the xx': 'Pib8eYDSFEI',
+    'bath|||offonoff': 'uCg-i8GEovI',
+    'espresso|||sabrina carpenter': 'eVli-tstM5E',
+    '24k magic|||bruno mars': 'UqyT8IEBkvY',
+    'i am|||ive': '6ZUIwj3FgUY',
+    'heya|||ive': '07EzMbVH3QE',
+    'fancy|||twice': 'kOHB85vDuow',
+    'boy with luv|||bts': 'XsX3ATc3FbA',
+    'levitating|||dua lipa': 'TUVcZfQe-Kw',
+    'super shy|||newjeans': 'ArmDp-zijuc',
+    'as it was|||harry styles': 'H5v3kku4y6Q',
+    'all of me|||john legend': '450p7goxZqg',
+    'a thousand years|||christina perri': 'rtOvBOTyX00'
+  });
+})();
