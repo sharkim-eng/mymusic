0facb2266691949ccb3037c1dd02eef83350ca20
sharkim-eng <waferman6@gmail.com>
2026-09-22 19:09:04 +0900

    Use high-resolution mobile shark background

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'shark-pwa-v9';
+const CACHE_NAME = 'shark-pwa-v10';
 const APP_SHELL = [
   './',
   './index.html',
@@ -6,6 +6,7 @@ const APP_SHELL = [
   './result.html',
   './playlist.html',
   './shark.css',
+  './shark-bg-hq.webp',
   './enhancements.css',
   './shark-core.js',
   './public/music-data.js',
