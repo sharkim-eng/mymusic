8722b8e6854ce1380769c2c551182503408abe2c
sharkim-eng <waferman6@gmail.com>
2026-09-22 20:20:33 +0900

    Restore shark background and refresh PWA cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'shark-pwa-v10';
+const CACHE_NAME = 'shark-pwa-v11';
 const APP_SHELL = [
   './',
   './index.html',
@@ -6,7 +6,6 @@ const APP_SHELL = [
   './result.html',
   './playlist.html',
   './shark.css',
-  './shark-bg-hq.webp',
   './enhancements.css',
   './shark-core.js',
   './public/music-data.js',
